25eb0f5b43e717ca28153e42e055cfa2700fae23
Rorius2022 <108790454+Rorius2022@users.noreply.github.com>
2024-02-28 16:52:57 +0200

    added index

---
+++ lessons/git/index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <title>Навчання IT</title>
+    <link rel="stylesheet"  href="/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&subset=cyrillic,cyrillic-ext" rel="stylesheet">
+    <meta charset="utf-8">
+</head><body>
+<div class="wrapper">
+    <div class="container">
+        <div class="default">
+            <div class="top">
+                <div class="back-arrow">
+                    <a href="/index.php"><img src="/images/back.png" width="30" height="30"></a>
+                </div>
+                <h1>Уроки Git</h1>
+            </div>
+            <div class="main">
+                <div>
+                    <img src="/images/js.png">
+                </div>
+                <div class="img left">
+                    <a href="lesson1">Урок 1. Вступ в Git</a><br>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
